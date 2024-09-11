6447f4b790e788dc7d08866e1deb7abca0fc148f
diananov11 <ohlemon0678@gmail.com>
2024-09-11 11:48:22 +0700

    add card for small device

--- mentor/mentor.js
+++ mentor/mentor.js
@@ -1,18 +1,15 @@
 let API = "https://66de9f23de4426916ee1b9e7.mockapi.io/Mentor";
 let cardLarge = document.getElementById("card-large");
+let cardSmall = document.getElementById("card-small");
 
-
-  let searchMentor = document.getElementById("search-mentor");
-searchMentor.addEventListener('keypress', handleSearch)
+let searchMentor = document.getElementById("search-mentor");
+searchMentor.addEventListener("keypress", handleSearch);
 
 function handleSearch(event) {
-  if (event.key === 'Enter') {
+  if (event.key === "Enter") {
     event.preventDefault();
-    
   }
 }
-
-
 
 // handle async for card-large-device
 async function getMentors() {
@@ -20,7 +17,7 @@ async function getMentors() {
   let mentors = await respons.json();
   mentors.map((item) => {
     cardLarge.innerHTML += `
-            <div class="card container d-none d-lg-block mb-3 shadow">
+            <div class="card container d-none d-lg-block mb-3 shadow" data-aos="fade-left" data-aos-duration="1500" >
               <div class="row">
                 <div class="col-5"><img
                     src="${item.avatar}"
@@ -40,6 +37,26 @@ async function getMentors() {
               </div>
             </div>
         `;
+    cardSmall.innerHTML += `<div class="card d-block mx-auto d-lg-none my-3" style="width: 15rem;" data-aos="fade-left" data-aos-duration="1500">
+                  <img
+                    src="${item.avatar}"
+                    class="card-img-top" alt="...">
+                  <div class="card-body text-center">
+                    <h5 class="card-title my-0">${item.name}</h5>
+                    <p class="my-1">${item.universitas}</p>
+                    <small class="d-block my-0">${item.asalDaerah}</small>
+                    <p class="card-text">
+                    </p>
+                    <div class="text-center">
+                      <a href="#" class="btn " id="btn-more">Info <i class="bi bi-arrow-right"></i></a>
+                      <a href="#" class="btn" id="btn-register">Daftar</a>
+                    </div>
+                  </div>
+                </div>`;
   });
 }
-getMentors()
+getMentors();
+
+//handle async card-small-device
+
+AOS.init();
