a19ab1cd5ddae290192d0df2837651c412eb4ac9
diananov11 <ohlemon0678@gmail.com>
2024-09-11 09:19:47 +0700

    add direktori

--- mentor/mentor.js
+++ mentor/mentor.js
@@ -1,16 +1,30 @@
 let API = "https://66de9f23de4426916ee1b9e7.mockapi.io/Mentor";
 let cardLarge = document.getElementById("card-large");
 
+
+  let searchMentor = document.getElementById("search-mentor");
+searchMentor.addEventListener('keypress', handleSearch)
+
+function handleSearch(event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    
+  }
+}
+
+
+
+// handle async for card-large-device
 async function getMentors() {
   let respons = await fetch(API);
   let mentors = await respons.json();
   mentors.map((item) => {
     cardLarge.innerHTML += `
-            <div class="card container d-none d-lg-block">
+            <div class="card container d-none d-lg-block mb-3 shadow">
               <div class="row">
                 <div class="col-5"><img
                     src="${item.avatar}"
-                    class="rounded img-fluid mt-3 d-flex " alt="..."></div>
+                    class="rounded img-fluid mt-4 d-flex " alt="..."></div>
                 <div class="col-7">
                   <div class="card-body">
                     <h5 class="card-title my-1">${item.name}</h5>
