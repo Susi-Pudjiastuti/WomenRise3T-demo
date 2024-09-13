21f06b5d00daab539e9fac63d5e0fc15e856b8da
EllenaAmanda <ellenaamanda846@gmail.com>
2024-09-13 20:35:50 +0700

    moved all footer and navbar to script.js, completed navigation routes, deleted excessive gambar folders

--- script.js
+++ script.js
@@ -1,3 +1,79 @@
+// Navigation Bar ==========================================================
+let navbar = document.getElementById("navbar")
+navbar.innerHTML =`<nav class="navbar navbar-expand-lg border-bottom">
+    <div class="container-fluid px-5 py-2 mx-5 d-flex gap-5">
+        
+            <div class="d-flex gap-2 align-items-center">
+                <img src="../gambar/Logo WomenRise3T.svg" width="48" height="auto">
+                <a href="../index.html"><div class="vesper-libre-bold pt-2" id="nav-title">WomenRise3T</div></a>
+            </div>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav navbar-text gap-5 flex-grow-1 ms-5">
+                    <li class="nav-item "><a href="../index.html">Home</a></li>
+                    <li class="nav-item"><a href="">Beasiswa</a></li>
+                    <li class="nav-item"><a href="/mentor/mentor.html">Mentor</a></li>
+                    <li class="nav-item"><a href="">Donasi</a></li>
+                </ul>
+
+                <div class="d-flex gap-2 align-items-center">
+                    <a class="btn button-style-outline px-3 py-2" href="">Log Out</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>`
+// ===================================================================================
+
+// Footer ============================================================================
+document.getElementById("footer").innerHTML = 
+`<footer class="border-top">
+        <div class="container px-5 px-md-0">
+            
+            <div class="row pt-5 pb-4 gy-4 ">
+                <div class="col-sm-4">
+                    <div class="vesper-libre-bold mb-3">
+                        <img src="../gambar/Logo WomenRise3T.svg" width="32" height="auto">
+                        WomenRise3T
+                    </div>
+                    <div class="text-body-tertiary">Jalan Inspirasi No. 123, Jakarta, Indonesia</div>
+                    <div class="text-body-tertiary mb-3">Telepon: +62 8123 456 7890</div>
+                    <a href=""><img src="../gambar/icon/facebook.svg" class="me-2" width="24" height="24"></a>
+                    <a href=""><img src="../gambar/icon/instagram.svg" width="24" height="24"></a>
+                </div>
+                    
+                <div class="col-sm col-md">
+                    <p>Get Help</p>
+                    <a class="text-body-tertiary" href=""><p>FAQ</p></a>
+                    <a class="text-body-tertiary" href=""><p>Contact us</p></a>
+                    <a class="text-body-tertiary" href=""><p>Support us</p></a>
+
+                </div>
+                <div class="col-sm col-md">
+                    <p>Our Program</p>
+                    <a class="text-body-tertiary" href=""><p>Mentorship</p></a>
+                    <a class="text-body-tertiary" href=""><p>Scholarship</p></a>
+                    <a class="text-body-tertiary" href=""><p>Community</p></a>
+                </div>
+                <div class="col-sm col-md">
+                    <p>Company</p>
+                    <a class="text-body-tertiary" href=""><p>About us</p></a>
+                    <a class="text-body-tertiary" href=""><p>Our Team</p></a>
+                    <a class="text-body-tertiary" href=""><p>Our Impact</p></a>
+                </div>
+            </div>
+            
+            <hr>
+            <p class="text-body-tertiary mb-5"> &copy; 2024 WomenRise3T. All Rights Reserved.</p>
+        </div>
+     </footer>`
+//===========================================================================================
+
+// Abous us Descriptions ====================================================================
 let aboutUs = {
     tentangWR3T: "WomenRise3T adalah platform yang didedikasikan untuk mendukung perempuan dari wilayah 3T (tertinggal, terdepan, terluar) dalam mengakses informasi beasiswa dan mendapatkan sesi mentoring eksklusif. Kami percaya bahwa setiap perempuan berhak mendapatkan kesempatan untuk berkembang melalui pendidikan. Oleh karena itu, kami menyediakan informasi beasiswa yang dapat diakses oleh siapa saja, serta menawarkan mentoring khusus untuk perempuan 3T yang telah terdaftar dan terverifikasi. Melalui WomenRise3T, kami berupaya memberikan akses yang lebih luas dan adil bagi perempuan marjinal untuk meraih pendidikan tinggi dan mewujudkan potensi mereka.",
     tentang2WR3T: "Platform WomenRise3T menawarkan basis data lengkap tentang peluang beasiswa yang dapat diakses oleh semua pengguna, memastikan informasi penting dapat dijangkau oleh siapa saja. WomenRise3T juga menawarkan sesi mentoring pribadi yang dirancang khusus untuk perempuan dari wilayah 3T yang telah terdaftar dan terverifikasi. Sesi ini bertujuan untuk memberikan bimbingan, dukungan, dan inspirasi dari mentor yang memiliki latar belakang serupa, sehingga perjalanan menuju pendidikan tinggi menjadi lebih relevan dan dapat dicapai."
@@ -8,3 +84,9 @@ let tentang2 = document.getElementById("tentang2")
 
 tentang1.textContent = aboutUs.tentangWR3T
 tentang2.textContent = aboutUs.tentang2WR3T
+
+// =================================================
+
+
+
+
